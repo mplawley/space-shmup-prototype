c3e56e94511463ebd67120b9c26d8c6bf3a4399f
mplawley <mark.lawley@gmail.com>
2016-09-23 14:25:47 -0400

    Shield functionality built

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour
+{
+	#region Fields
+
+	public float rotationsPerSecond = 0.1f;
+	public int levelShown = 0;
+
+	#endregion
+
+	#region Methods
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		//Read the current shield level from the Hero singleton
+		int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+
+		//If this is different from levelShown...
+		if (levelShown != currLevel)
+		{
+			levelShown = currLevel;
+			Material mat = this.GetComponent<Renderer>().material;
+
+			//Adjust the texture offset to show different shield level
+			mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);
+		}
+
+		//Rotate the shield a bit every second
+		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+		transform.rotation = Quaternion.Euler(0, 0, rZ);
+	}
+
+	#endregion
+}
